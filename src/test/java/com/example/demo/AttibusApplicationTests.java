15f1f1fa4a9c5e0a9629a45e789ba927304ed03c
mina kim <mina14712@gmail.com>
2024-06-25 14:32:49 +0900

    newBranch

--- src/test/java/com/example/demo/AttibusApplicationTests.java
+++ src/test/java/com/example/demo/AttibusApplicationTests.java
@@ -3,24 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.example.demo.testEntity.TestName;
-import com.example.demo.testReopository.TestRepository;
+
 
 @SpringBootTest
 class AttibusApplicationTests {
 
-	@Autowired
-	TestRepository repository;
 
 	@Test
 	void contextLoads() {
-		System.out.println("hello world!");
-	}
 
-//	@Test
-//	void 엔티티연결됐나요(){
-//		TestName name = TestName.builder().name("??").build();
-//		repository.save(name);
-//	}
+		System.out.println("HELLO WORLD");
+	}
 
 }
