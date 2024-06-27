72d7cad8ea24a74532d772c79d6dc8fb1a2112e3
kimys <kimyslive7@naver.com>
2024-06-27 10:12:42 +0900

    중간푸쉬

--- src/main/java/com/example/demo/Landmark/entity/Landmark_tag.java
+++ src/main/java/com/example/demo/Landmark/entity/Landmark_tag.java
@@ -1,25 +1,25 @@
-package com.example.demo.Landmark.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Landmark_tag {
-
-    //  여행지 고유값 (외래키)
-    @Id
-    @Column(length = 11, nullable = false)
-    int landmark_no;
-
-    //  태그네임
-    @Column(length = 11, nullable = false)
-    String tag_name;
-}
+//package com.example.demo.Landmark.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.*;
+//
+//@Entity
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class Landmark_tag {
+//
+//    //  여행지 고유값 (외래키)
+//    @Id
+//    @Column(length = 11, nullable = false)
+//    int landmark_no;
+//
+//    //  태그네임
+//    @Column(length = 11, nullable = false)
+//    String tag_name;
+//}
--- src/main/java/com/example/demo/Landmark/repository/Landmark_tegRepository.java
+++ src/main/java/com/example/demo/Landmark/repository/Landmark_tegRepository.java
@@ -1,9 +1,9 @@
-package com.example.demo.Landmark.repository;
-
-import com.example.demo.Landmark.entity.Landmark_tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.io.Serializable;
-
-public interface Landmark_tegRepository extends JpaRepository<Landmark_tag, Serializable> {
-}
+//package com.example.demo.Landmark.repository;
+//
+//import com.example.demo.Landmark.entity.Landmark_tag;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.io.Serializable;
+//
+//public interface Landmark_tegRepository extends JpaRepository<Landmark_tag, Serializable> {
+//}
