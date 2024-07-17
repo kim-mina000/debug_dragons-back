8ca5053415bbe157a2fcd2564c17bcc60c944455
mina kim <mina14712@gmail.com>
2024-07-17 12:24:41 +0900

    ddd

--- src/test/java/com/example/demo/repository/LandMarkRepositoryTest.java
+++ src/test/java/com/example/demo/repository/LandMarkRepositoryTest.java
@@ -1,49 +1,49 @@
-package com.example.demo.repository;
-
-import com.example.demo.Landmark.entity.Landmark;
-import com.example.demo.Landmark.repository.LandmarkRepository;
-import com.example.demo.member.entity.Member;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalTime;
-import java.util.Optional;
-
-@SpringBootTest
-public class LandMarkRepositoryTest {
-
-    @Autowired
-    LandmarkRepository repository;
-
-    @Test
-    public void 저장해보기(){
-        Landmark landmark = Landmark.builder()
-                .landmarkAddress("냐냐냥")
-                .landmarkName("냥뇽녕")
-                .landmarkOrigin(true)
-                .latitude("123")
-                .longitude("123")
-                .landmarkDay(10)
-                .writer(Member.builder().userId("11").userPw("1234").userName("11").userRole("ROLE_USER").build())
-                .landmarkTime(LocalTime.of(10,10,10))
-                .build();
-
-        repository.save(landmark);
-    }
-
-    @Test
-    public void 수정하기(){
-        Optional<Landmark> result = repository.findById(1);
-        Landmark landmark = result.get();
-        landmark.setLandmarkDay(10);
-        landmark.setLandmarkTime(LocalTime.of(18,18));
-        repository.save(landmark);
-    }
-
-    @Test
-    public void 주소로랜드마크찾기(){
-        Landmark landmark = repository.selectByAddress("대전 유성구 안산동 산 28-2").get();
-        System.out.println(landmark);
-    }
-}
+//package com.example.demo.repository;
+//
+//import com.example.demo.Landmark.entity.Landmark;
+//import com.example.demo.Landmark.repository.LandmarkRepository;
+//import com.example.demo.member.entity.Member;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.time.LocalTime;
+//import java.util.Optional;
+//
+//@SpringBootTest
+//public class LandMarkRepositoryTest {
+//
+//    @Autowired
+//    LandmarkRepository repository;
+//
+//    @Test
+//    public void 저장해보기(){
+//        Landmark landmark = Landmark.builder()
+//                .landmarkAddress("냐냐냥")
+//                .landmarkName("냥뇽녕")
+//                .landmarkOrigin(1)
+//                .latitude("123")
+//                .longitude("123")
+//                .landmarkDay(10)
+//                .writer(Member.builder().userId("11").userPw("1234").userName("11").userRole("ROLE_USER").build())
+//                .landmarkTime(LocalTime.of(10,10,10))
+//                .build();
+//
+//        repository.save(landmark);
+//    }
+//
+//    @Test
+//    public void 수정하기(){
+//        Optional<Landmark> result = repository.findById(1);
+//        Landmark landmark = result.get();
+//        landmark.setLandmarkDay(10);
+//        landmark.setLandmarkTime(LocalTime.of(18,18));
+//        repository.save(landmark);
+//    }
+//
+//    @Test
+//    public void 주소로랜드마크찾기(){
+//        Landmark landmark = repository.selectByAddress("대전 유성구 안산동 산 28-2").get();
+//        System.out.println(landmark);
+//    }
+//}
--- src/test/java/com/example/demo/service/LandMarkServiceTest.java
+++ src/test/java/com/example/demo/service/LandMarkServiceTest.java
@@ -1,79 +1,79 @@
-package com.example.demo.service;
-
-import com.example.demo.Landmark.dto.LandmarkDTO;
-import com.example.demo.Landmark.service.LandmarkService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalTime;
-import java.util.List;
-
-@SpringBootTest
-public class LandMarkServiceTest {
-
-    @Autowired
-    LandmarkService service;
-
-    @Test
-    public void 서비스(){
-        LandmarkDTO dto = LandmarkDTO.builder()
-                .landmarkNo(0)
-                .writer("bbq5883")
-                .landmarkDay(2)
-                .landmarkTime(LocalTime.of(12,30))
-                .landmarkAddress("인천")
-                .landmarkName("월미도")
-                .landmarkOrigin(true)
-                .landmarkShortDesc("회먹고산책하고")
-                .landmarkDesc("중구월미도에요")
-                .longitude("543")
-                .latitude("123")
-                .build();
-
-        LandmarkDTO no = service.register(dto);
-
-        System.out.println("새로 입력된 게시물 넘버" + no);
-
-    }
-
-    @Test
-    public void 리스트줘(){
-        List list = service.getList();
-        System.out.println(list);
-    }
-
-    @Test
-    public void 수정해죠(){
-        LandmarkDTO landmark = LandmarkDTO.builder()
-                .landmarkNo(1)
-                .landmarkDay(9 )
-                .landmarkTime(LocalTime.of(9,10,10))
-                .writer("11")
-                .landmarkAddress("시리네집")
-                .landmarkName("호엥")
-                .longitude("123456")
-                .latitude("1231455")
-                .landmarkOrigin(true)
-                .build();
-
-        service.register(landmark);
-    }
-
-    @Test
-    public void 아이디로리스트찾아줘(){
-        List<LandmarkDTO> list = service.getListById("11");
-        System.out.println(list);
-    }
-
-    @Test
-    public void 삭제테스트(){
-        service.remove(5);
-    }
-
-    @Test
-    public void 주소로접근(){
-        LandmarkDTO result = service.readBylandmarkAddress("");
-    }
-
-}
+//package com.example.demo.service;
+//
+//import com.example.demo.Landmark.dto.LandmarkDTO;
+//import com.example.demo.Landmark.service.LandmarkService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.time.LocalTime;
+//import java.util.List;
+//
+//@SpringBootTest
+//public class LandMarkServiceTest {
+//
+//    @Autowired
+//    LandmarkService service;
+//
+//    @Test
+//    public void 서비스(){
+//        LandmarkDTO dto = LandmarkDTO.builder()
+//                .landmarkNo(0)
+//                .writer("bbq5883")
+//                .landmarkDay(2)
+//                .landmarkTime(LocalTime.of(12,30))
+//                .landmarkAddress("인천")
+//                .landmarkName("월미도")
+//                .landmarkOrigin(1)
+//                .landmarkShortDesc("회먹고산책하고")
+//                .landmarkDesc("중구월미도에요")
+//                .longitude("543")
+//                .latitude("123")
+//                .build();
+//
+//        LandmarkDTO no = service.register(dto);
+//
+//        System.out.println("새로 입력된 게시물 넘버" + no);
+//
+//    }
+//
+//    @Test
+//    public void 리스트줘(){
+//        List list = service.getList();
+//        System.out.println(list);
+//    }
+//
+//    @Test
+//    public void 수정해죠(){
+//        LandmarkDTO landmark = LandmarkDTO.builder()
+//                .landmarkNo(1)
+//                .landmarkDay(9 )
+//                .landmarkTime(LocalTime.of(9,10,10))
+//                .writer("11")
+//                .landmarkAddress("시리네집")
+//                .landmarkName("호엥")
+//                .longitude("123456")
+//                .latitude("1231455")
+//                .landmarkOrigin(1)
+//                .build();
+//
+//        service.register(landmark);
+//    }
+//
+//    @Test
+//    public void 아이디로리스트찾아줘(){
+//        List<LandmarkDTO> list = service.getListById("11");
+//        System.out.println(list);
+//    }
+//
+//    @Test
+//    public void 삭제테스트(){
+//        service.remove(5);
+//    }
+//
+//    @Test
+//    public void 주소로접근(){
+//        LandmarkDTO result = service.readBylandmarkAddress("");
+//    }
+//
+//}
